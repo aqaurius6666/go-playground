5b03d4c1262867960c6a77f0d14355c97796d776
Vu Nguyen <aqaurius6666@gmail.com>
2021-06-30 08:19:22 +0700

    folder structure

--- src/main.go
+++ src/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/aquarius6666/go-playground/src/greeting"
+	"github.com/aquarius6666/go-playground/src/mymath"
 )
 
 func main() {
 	fmt.Println("Hello World")
 	greeting.Hello()
+	ans := mymath.Sum(3.4, -5.4)
+	fmt.Println(ans)
 }
---
+++ src/mymath/mymath.go
@@ -0,0 +1,9 @@
+package mymath
+
+import (
+	"math"
+)
+
+func Sum(a float64, b float64) float64 {
+	return math.Abs(a) + math.Abs(b)
+}
